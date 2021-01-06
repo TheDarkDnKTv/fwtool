f7eb000bb4bdacbc1a2de5090a7ace7daadf1378
TheDarkDnKTv <x22dnk@hotmail.com>
2021-01-06 20:17:25 +0200

    Initial commit

---
+++ src/thedarkdnktv/fwtool/FWTool.java
@@ -0,0 +1,12 @@
+package thedarkdnktv.fwtool;
+
+/**
+ * @author TheDarkDnKTv
+ *
+ */
+public class FWTool {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
